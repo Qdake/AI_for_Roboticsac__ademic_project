624fe63d6c7392ea42ecbe509e69731b81f2da1a
FelixKleinpeter <37071597+FelixKleinpeter@users.noreply.github.com>
2019-12-20 12:57:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     # init gym
     env = gym.make('FastsimSimpleNavigation-v0')
     # type d'algo dispo
-    assert algo_name in ['SHINE','MAPelites','MAPelites_ns','NS']
+    assert algo_name in ['SHINE','MAPelites','NS']
     # diff cas
     if algo_name == 'SHINE':
         position_record, qtree, nb_gen_found = SHINE(env,nb_generation=nb_generation, size_pop=size_pop,pb_crossover=pb_crossover,pb_mutation=pb_mutation,display=display)
@@ -44,8 +44,6 @@ if __name__ == "__main__":
         position_record, grid, nb_gen_found = MAPelites_curiosite(env,nb_generation=nb_generation, size_pop=size_pop,pb_crossover=pb_crossover,pb_mutation=pb_mutation,display=display)
     if algo_name == 'NS':
         position_record, arc, nb_gen_found = novelty_search(env,nb_generation=nb_generation, size_pop=size_pop,pb_crossover=pb_crossover,pb_mutation=pb_mutation,display=display)
-    if algo_name == 'MAPelites_ns':
-        position_record, grid, nb_gen_found = MAPelites_ns(env,nb_generation=nb_generation, size_pop=size_pop,pb_crossover=pb_crossover,pb_mutation=pb_mutation,display=display)
     # fermer gym
     env.close()
     
@@ -73,10 +71,10 @@ if __name__ == "__main__":
 
     pr = pickle.load( open( nfolder+nfile, "rb" ) )
     position_record = pr[0]
-    qtree = pr[2]
     plot_position_record(position_record,nfolder,nimg)  #plot and save
     # qtree
     if algo_name == 'SHINE':
+        qtree = pr[2]
         plot_qtree(qtree,nfolder,nimg)  #plot and save
 
     plt.title(algo_name)
